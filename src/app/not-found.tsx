6ab71f834a088102e79c9697be0a329454240c8c
LucasACH <achaval.lucas@gmail.com>
2024-06-13 19:56:32 -0300

    Remove unused code and components

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
+      <h2>404 Not Found</h2>
+    </div>
+  );
+}
